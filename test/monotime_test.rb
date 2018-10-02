000972c022dd91c8bcd439451e65b7485931ef44
Thomas Hurst <tom@hur.st>
2018-10-02 23:47:59 +0100

    Duration#to_s: More tests

--- test/monotime_test.rb
+++ test/monotime_test.rb
@@ -36,14 +36,23 @@ class MonotimeTest < Minitest::Test
     assert_equal '1s', Duration.from_secs(1).to_s
     assert_equal '1.5s', Duration.from_secs(1.5).to_s
     assert_equal '1.25s', Duration.from_secs(1.25).to_s
+    assert_equal '1.2s', Duration.from_secs(1.25).to_s(1)
+    assert_equal '1.3s', Duration.from_secs(1.26).to_s(1)
+    assert_equal '2s', Duration.from_secs(1.6).to_s(0)
 
     assert_equal '1ms', Duration.from_millis(1).to_s
     assert_equal '1.5ms', Duration.from_millis(1.5).to_s
     assert_equal '1.25ms', Duration.from_millis(1.25).to_s
+    assert_equal '1.2ms', Duration.from_millis(1.25).to_s(1)
+    assert_equal '1.3ms', Duration.from_millis(1.26).to_s(1)
+    assert_equal '2ms', Duration.from_millis(1.6).to_s(0)
 
     assert_equal '1μs', Duration.from_micros(1).to_s
     assert_equal '1.5μs', Duration.from_micros(1.5).to_s
     assert_equal '1.25μs', Duration.from_micros(1.25).to_s
+    assert_equal '1.2μs', Duration.from_micros(1.25).to_s(1)
+    assert_equal '1.3μs', Duration.from_micros(1.26).to_s(1)
+    assert_equal '2μs', Duration.from_micros(1.6).to_s(0)
 
     assert_equal '1ns', Duration.from_nanos(1).to_s
   end
